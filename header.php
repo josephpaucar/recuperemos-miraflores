185d852517d7ebe5aad48d6a63452c327330f1ac
josephpaucar <mailme@josephpaucar.dev>
2022-12-29 19:41:01 -0500

    cambios en el header y el style.css

--- header.php
+++ header.php
@@ -7,17 +7,3 @@
     <?php wp_head() ?>
 </head>
 <body>
-    
-<!-- <nav class="relative container mx-auto p-6 bg-primary">
-    <div class="flex items-center justify-between">
-        <div class="pt-2">
-            <h2 class="font-display text-secondary font-bold">RecuperemosMiraflores</h2>
-        </div>
-        <div class="hidden md:flex space-x-6 font-body">
-            <a class="hover:text-mainWhite" href="#">Pricing</a>
-            <a class="hover:text-mainWhite" href="#">Product</a>
-            <a class="hover:text-mainWhite" href="#">About us</a>
-            <a class="hover:text-mainWhite" href="#">Community</a>
-        </div>
-    </div>
-</nav> -->
