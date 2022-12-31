9f68166d95e9462705697160538ffbc54a8c10c1
josephpaucar <mailme@josephpaucar.dev>
2022-12-31 00:15:39 -0500

    correcciones ortográficas

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <div class="bg-primary w-full h-auto lg:col-start-6 md:col-start-5 md:col-end-9 lg:col-end-9 text-secondary text-center">
             <div class="bg-ultradark p-6">
                 <h3 class="text-2xl font-semibold py-4">Estimados vecinos miraflorinos:</h3>
-                <p class="text-2xl font-normal my-4">En este portal pueden enviar sus pedidos quejas, denuncias o propuestas que consideren aportarán a mejorar nuestro distrito, pueden enviar fotos, videos o audios que sustenten su reporte, nosotros nos encargaremos como plataforma vecinal de hacerlas llegar a las autoridades respectivas, ademàs contamos con nuestra fan page con más de 24 mil seguidores donde difundiremos los temas de relevancia.</p>
+                <p class="text-2xl font-normal my-4">En este portal pueden enviar sus pedidos, quejas, denuncias o propuestas que consideren aportarán a mejorar nuestro distrito, pueden enviar fotos, videos o audios que sustenten su reporte, nosotros nos encargaremos como plataforma vecinal de hacerlas llegar a las autoridades respectivas, además contamos con nuestra fan page con más de 24 mil seguidores donde difundiremos los temas de relevancia.</p>
             </div>
             <div class="p-6">
                 <h2 class="text-3xl">Vecino reporta</h2>
@@ -49,7 +49,7 @@
         <div class="w-full h-full flex flex-col text-primary p-6 md:w-2/4">
             <h2 class="text-5xl font-semibold leading-tight mb-4">Reportes Vecinales</h2>
             <div class="underline w-2/4"></div>
-            <p class="text-lg my-6">Estimados vecinos, ser escuchados y participar sin buscar protagonismo es una premisa muy valiosa para nosotros para apoyar a la sociedad, por ello queremos ayudar en que los pedidos, quejas, denuncias o propuestas que consideres aportaràn a mejorar nuestro distrito sean atendidas por las autoridades respectivas.</p>
+            <p class="text-lg my-6">Estimados vecinos, ser escuchados y participar sin buscar protagonismo es una premisa muy valiosa para nosotros para apoyar a la sociedad, por ello queremos ayudar en que los pedidos, quejas, denuncias o propuestas que consideres aportarán a mejorar nuestro distrito sean atendidas por las autoridades respectivas.</p>
             <h2 class="text-3xl text-center">Vecino reporta</h2>
             <a href="#reporta" class="block text-center bg-ligth text-xl text-primary mt-2 mb-3 p-3 w-64 mx-auto" href="#formulario-reporte">Enviar reporte</a>
         </div>
@@ -67,7 +67,7 @@
         <div class="w-full h-full flex flex-col text-primary p-6 md:w-2/4">
             <h2 class="text-5xl tracking-wide leading-tight mb-4">Sustenta tu información</h2>
             <div class="underline w-3/5"></div>
-            <p class="text-lg my-6">Estimados vecinos, puedes enviarnos tus pedidos, quejas, renuncias o propuestas que consideres aportarán a mejorar nuestro distrito, con sustento veráz adjuntando información que sea evidencia del reporte.</p>
+            <p class="text-lg my-6">Estimados vecinos, puedes enviarnos tus pedidos, quejas, renuncias o propuestas que consideres aportarán a mejorar nuestro distrito, con sustento veraz adjuntando información que sea evidencia del reporte.</p>
         </div>
         <div class="w-full h-full flex flex-col justify-center text-primary p-6 md:w-2/4">
             <button class="bg-ligth text-xl text-primary mt-2 mb-3 p-3 w-64 mx-auto rounded-full" href="#formulario-reporte">fotos</button>
